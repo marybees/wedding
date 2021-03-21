8cd29cbd1b8d0cd57364b8c6a14e95aaa1616443
Mary Baker <meb601@gmail.com>
2021-03-20 23:49:14 -0400

    Add remaining homepage headings.

--- src/App.js
+++ src/App.js
@@ -11,15 +11,31 @@ function App() {
         </Col>
       </Row>
       <Row>
-        <Col span={8} style={{ fontFamily: 'Cormorant Unicase' }}>
+        <Col className='title' span={8}>
           Welcome Clambake
         </Col>
-        <Col span={8} style={{ fontFamily: 'Cormorant Unicase' }}>
+        <Col className='title' span={8}>
           Wedding
         </Col>
-        <Col span={8} style={{ fontFamily: 'Cormorant Unicase' }}>
-          Beach <span style={{ fontFamily: 'Sacramento' }}>and</span> Goodbyes
+        <Col className='title' span={8}>
+          Beach <span className='subtitle'>and</span> Goodbyes
         </Col>
+      </Row>
+      <Row>
+        <Col className='title' span={12}>Where <span className='subtitle'>to</span> Stay</Col>
+        <Col className='title' span={12}>Getting There</Col>
+      </Row>
+      <Row>
+        <Col className='title' span={12}>Our Story</Col>
+        <Col className='title' span={12}>Registry</Col>
+      </Row>
+      <Row>
+        <Col className='title' span={12}>Request <span className='subtitle'>a</span> Song</Col>
+        <Col className='title' span={12}>Share <span className='subtitle'>a</span> Memory</Col>
+      </Row>
+      <Row>
+        <Col className='title' span={12}>Things <span className='subtitle'>to</span> Do <span className='subtitle'>ongit</span> Cape Cod</Col>
+        <Col className='title' span={12}>COVID-19</Col>
       </Row>
     </div>
   );

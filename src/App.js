a69392189bebd730975159572587f4a0d1866242
Mary Baker <meb601@gmail.com>
2021-03-21 00:34:06 -0400

    Add schedules.

--- src/App.js
+++ src/App.js
@@ -16,16 +16,23 @@ function App() {
           <p style={{ fontStyle: 'italic' }}>for out of town guests</p>
           <p className='date'>Friday, September 3, 2021</p>
           <a href='https://www.google.com/maps/place/20+Roos+Rd,+Mashpee,+MA+02649/@41.7477327,-70.4565802,17z/data=!4m13!1m7!3m6!1s0x89e4cb21dd21f7b9:0x72a40c7a07b75002!2s20+Roos+Rd,+Mashpee,+MA+02649!3b1!8m2!3d41.7477327!4d-70.4543915!3m4!1s0x89e4cb21dd21f7b9:0x72a40c7a07b75002!8m2!3d41.7477327!4d-70.4543915' target='blank'><p>20 Roos Road, East Sandwich, MA 02537</p></a>
+          <p>5:00 PM</p>
         </Col>
         <Col className='subtitle' span={8}>
           <h2>Wedding</h2>
           <p className='date'>Saturday, September 4, 2021</p>
           <a href='https://www.google.com/maps/place/20+Roos+Rd,+Mashpee,+MA+02649/@41.7477327,-70.4565802,17z/data=!4m13!1m7!3m6!1s0x89e4cb21dd21f7b9:0x72a40c7a07b75002!2s20+Roos+Rd,+Mashpee,+MA+02649!3b1!8m2!3d41.7477327!4d-70.4543915!3m4!1s0x89e4cb21dd21f7b9:0x72a40c7a07b75002!8m2!3d41.7477327!4d-70.4543915' target='blank'><p>20 Roos Road, East Sandwich, MA 02537</p></a>
+          <p>2:00 PM: Ceremony</p>
+          <p>3:00 PM: Cocktail Hour</p>
+          <p>5:00 PM: Dinner</p>
+          <p>6:00 PM: Band</p>
+          <p>9:00 - 11:00 PM: DJ</p>
         </Col>
         <Col className='subtitle' span={8}>
           <h2>Beach <span>and</span> Goodbyes</h2>
           <p className='date'>Sunday, September 5, 2021</p>
           <a href='https://www.google.com/maps/place/Sandy+Neck+Beach/@41.7389773,-70.3832071,17z/data=!3m1!4b1!4m5!3m4!1s0x89fb3577aac617a3:0xd50789a765c3e50d!8m2!3d41.7389773!4d-70.3810184' target='blank'><p>Sandy Neck Beach</p></a>
+          <p>12:00 PM</p>
         </Col>
       </Row>
       <Row>

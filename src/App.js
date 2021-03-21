3953b78c6427a1915239375f3985ec62aba4d503
Mary Baker <meb601@gmail.com>
2021-03-20 23:39:44 -0400

    Add wedding section layout and titles.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './index.less'
+import { Row, Col } from 'antd';
 
 function App() {
+
   return (
-    <div className="App">
-      <h1 className='title'>Mary <span>and</span> Will</h1>
+    <div>
+      <Row>
+        <Col span={24}>
+          <h1 className='title'>Mary <span>and</span> Will</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={8} style={{ fontFamily: 'Cormorant Unicase' }}>
+          Welcome Clambake
+        </Col>
+        <Col span={8} style={{ fontFamily: 'Cormorant Unicase' }}>
+          Wedding
+        </Col>
+        <Col span={8} style={{ fontFamily: 'Cormorant Unicase' }}>
+          Beach <span style={{ fontFamily: 'Sacramento' }}>and</span> Goodbyes
+        </Col>
+      </Row>
     </div>
   );
 }

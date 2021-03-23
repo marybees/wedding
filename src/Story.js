48cce3f73ee0e450ae891596bb881adcf2e3736c
Mary Baker <meb601@gmail.com>
2021-03-23 16:23:48 -0400

    Update Story with Youtube video and description.

--- src/Story.js
+++ src/Story.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import YoutubeEmbed from "./YoutubeEmbed";
 
 function Story() {
     return (
-        <div style={{padding: '8rem 2rem 2rem 2rem'}}>
+        <div style={{padding: '8rem 2rem 2rem 2rem', textAlign: 'center'}}>
             <h1 className='pageTitle'>Our Story</h1>
+            <h3>Will and Mary met in Brazil in 2018.</h3>
+            <h3 style={{ padding: '2rem 0'}}>Will made this video for Mary for Christmas 2019. It was originally intended as a satirical take on <a href='https://traveler.marriott.com/storybooked/'>Marriott's Storybooked series</a>, but it morphed into something much more special than that. The video showcases real video and drone footage from their travels together. Countries featured include Spain, Greece, Croatia, Bosnia and Herzegovina, Costa Rica,  France, Indonesia, Italy, Mexico, and Canada.</h3>
+            <YoutubeEmbed embedId="rokGy0huYEA" />
         </div>
     )
 }
---
+++ src/YoutubeEmbed.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const YoutubeEmbed = ({ embedId }) => (
+  <div className="video-responsive">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/A3SjMN7UBec" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </div>
+);
+
+YoutubeEmbed.propTypes = {
+    embedId: PropTypes.string.isRequired
+  };
+  
+  export default YoutubeEmbed;

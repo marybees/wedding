2c8fbf3488a0aa36ad981d1c512cceb971b99050
Mary Baker <meb601@gmail.com>
2021-03-23 16:53:48 -0400

    Add registry details.

--- src/Registry.js
+++ src/Registry.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 function Registry() {
     return (
-        <div style={{padding: '8rem 2rem 2rem 2rem'}}>
+        <div style={{padding: '8rem 2rem 2rem 2rem', textAlign: 'center'}}>
             <h1 className='pageTitle'>Registry</h1>
+            <h3><a href='https://www.amazon.com/wedding/share/twentyroosroad'>Amazon</a></h3>
+            <h3><a href='https://www.williams-sonoma.com/registry/7wflljnlm6/registry-list.html'>Williams Sonoma, Pottery Barn, & West Elm</a></h3>
+            <h3><a href='https://www.wayfair.com/registry/wedding/4a939a1b-21da-4d5c-8cdc-702be8b7e1c3'>Wayfair</a></h3>
         </div>
     )
 }

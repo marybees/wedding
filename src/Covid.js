3e71a9d2cf32445e8d85503750ad6496c4fdbbbf
Mary Baker <meb601@gmail.com>
2021-03-23 19:14:26 -0400

    Add COVID-19 messaging.

--- src/Covid.js
+++ src/Covid.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 function Covid() {
     return (
-        <div style={{padding: '8rem 2rem 2rem 2rem'}}>
+        <div style={{padding: '8rem 2rem 2rem 2rem', textAlign: 'center' }}>
             <h1 className='pageTitle'>COVID-19</h1>
+            <p>Due to the number of guests we have coming from out of the country, we kindly ask those who have not received a COVID-19 vaccine to take a rapid COVID test the day prior to the wedding.</p>
+            <p><a href='https://www.capecodhealth.org/locations/profile/cape-cod-healthcare-urgent-care-sandwich/?utm_source=YextGoogle&utm_medium=Organic&utm_campaign=Website' target='_blank'>Schedule a Local Rapid Test</a></p>
+            <p><a href='https://www.cdc.gov/coronavirus/2019-ncov/index.html' target='_blank'>Center for Disease Control and Prevention</a></p>
         </div>
     )
 }

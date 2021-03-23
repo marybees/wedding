9893f4eb0bfea9fa14facd7e8bf761fa8989dd59
Mary Baker <meb601@gmail.com>
2021-03-23 18:44:01 -0400

    Update Memory with text and photo.

--- src/Memory.js
+++ src/Memory.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Image, Row, Col } from 'antd';
+import ScubaPic from './scuba-diving.JPG'
 
 function Memory() {
+
     return (
         <div style={{padding: '8rem 2rem 2rem 2rem', textAlign: 'center'}}>
             <h1 className='pageTitle'>Share <span>a</span> Memory</h1>
+            <Row>
+                <Col span={24} style={{ paddingBottom: '2rem'}}>
+                    <Image
+                    width={300}
+                    src={ScubaPic}
+                    />
+                </Col>
+            </Row>
             <h3 style={{ fontSize: '1.5rem'}}>Share your favorite memory with the bride and/or groom.</h3>
             <p><a href='' target='_blank'>Upload a photo or video</a> for our wedding slideshow!</p>
         </div>

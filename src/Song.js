0403257cbbaaed28a7d9bd1b73231c97b7338558
Mary Baker <meb601@gmail.com>
2021-03-23 17:50:11 -0400

    Add Google Form to Song.

--- src/Song.js
+++ src/Song.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 function Song() {
+
     return (
         <div style={{padding: '8rem 2rem 2rem 2rem'}}>
             <h1 className='pageTitle'>Request <span>a</span> Song</h1>
+            <div style={{ textAlign: 'center'}}>
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSct2fXnwq-m0lmvBxJpuvUSCGSpttUeX8Kr2UkrTi3lBULELA/viewform?embedded=true" width="640" height="685" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            </div>
         </div>
     )
 }
 
-export default Song
+export default Song;

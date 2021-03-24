939749f8be08625b1b257133a7c663414dfb6480
Mary Baker <meb601@gmail.com>
2021-03-24 08:44:55 -0400

    Update Story font.

--- src/Story.js
+++ src/Story.js
@@ -5,8 +5,7 @@ function Story() {
     return (
         <div style={{padding: '8rem 2rem 2rem 2rem', textAlign: 'center'}}>
             <h1 className='pageTitle'>Our Story</h1>
-            <h3>Will and Mary met in Brazil in 2018.</h3>
-            <h3 style={{ padding: '2rem 0'}}>Will made this video for Mary for Christmas 2019. It was originally intended as a satirical take on <a href='https://traveler.marriott.com/storybooked/'>Marriott's Storybooked series</a>, but it morphed into something much more special than that. The video showcases real video and drone footage from their travels together. Countries featured include Spain, Greece, Croatia, Bosnia and Herzegovina, Costa Rica,  France, Indonesia, Italy, Mexico, and Canada.</h3>
+            <p style={{ padding: '2rem 0'}}>Will made this video for Mary for Christmas 2019. It was originally intended as a satirical take on <a href='https://traveler.marriott.com/storybooked/'>Marriott's Storybooked series</a>, but it morphed into something much more special than that. The video showcases real video and drone footage from their travels together. Countries featured include Spain, Greece, Croatia, Bosnia and Herzegovina, Costa Rica,  France, Indonesia, Italy, Mexico, and Canada.</p>
             <YoutubeEmbed embedId="rokGy0huYEA" />
         </div>
     )

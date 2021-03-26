8e382cf2388e60fe789777e355bb2209ed856fb8
Mary Baker <meb601@gmail.com>
2021-03-26 09:32:24 -0400

    Add registry message.

--- src/Registry.js
+++ src/Registry.js
@@ -4,6 +4,7 @@ function Registry() {
     return (
         <div style={{padding: '8rem 2rem 2rem 2rem', textAlign: 'center'}}>
             <h1 className='pageTitle'>Registry</h1>
+            <p>Your presence alone is our present! If you feel inclined to also give a gift, here is a list of retailers we are registered at:</p>
             <h3><a href='https://www.amazon.com/wedding/share/twentyroosroad' target="_blank">Amazon</a></h3>
             <h3><a href='https://www.williams-sonoma.com/registry/7wflljnlm6/registry-list.html' target="_blank">Williams Sonoma, Pottery Barn, & West Elm</a></h3>
             <h3><a href='https://www.wayfair.com/registry/wedding/4a939a1b-21da-4d5c-8cdc-702be8b7e1c3' target="_blank">Wayfair</a></h3>

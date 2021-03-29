6e7a035a7967504ee388e975e5ad943ddb3d8dff
Will Washburn <will.washburn@gmail.com>
2021-03-29 10:32:26 -0400

    Scroll to top when changing route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './index.less';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
+  useLocation
 } from "react-router-dom";
 import Accommodation from './Accommodation';
 import Directions from './Directions';
@@ -17,9 +18,20 @@ import Footer from './Footer';
 import Activities from './Activities';
 import Covid from './Covid';
 
+export function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div>
         <Header/>
         <Switch>

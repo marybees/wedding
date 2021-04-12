46638b3aad8f417f65bad29a8b73059faf2cd26a
Mary Baker <meb601@gmail.com>
2021-04-12 19:57:12 -0400

    Add Pottery Barn and West Elm registry links.

--- src/Registry.js
+++ src/Registry.js
@@ -5,9 +5,11 @@ function Registry() {
         <div style={{padding: '8rem 2rem 2rem 2rem', textAlign: 'center'}}>
             <h1 className='pageTitle'>Registry</h1>
             <p>Your presence alone is our present! If you feel inclined to also give a gift, here is a list of retailers we are registered at:</p>
-            <h3><a href='https://www.amazon.com/wedding/share/twentyroosroad' target="_blank">Amazon</a></h3>
-            <h3><a href='https://www.williams-sonoma.com/registry/7wflljnlm6/registry-list.html' target="_blank">Williams Sonoma, Pottery Barn, & West Elm</a></h3>
-            <h3><a href='https://www.wayfair.com/registry/wedding/4a939a1b-21da-4d5c-8cdc-702be8b7e1c3' target="_blank">Wayfair</a></h3>
+            <h3><a href='https://www.amazon.com/wedding/share/twentyroosroad' target="blank">Amazon</a></h3>
+            <h3><a href='https://www.williams-sonoma.com/registry/7wflljnlm6/registry-list.html' target="blank">Williams Sonoma</a></h3>
+            <h3><a href='https://www.potterybarn.com/registry/7wflljnlm6/registry-list.html' target='blank'>Pottery Barn</a></h3>
+            <h3><a href='https://www.westelm.com/registry/7wflljnlm6/registry-list.html' target='blank'>West Elm</a></h3>
+            <h3><a href='https://www.wayfair.com/registry/wedding/4a939a1b-21da-4d5c-8cdc-702be8b7e1c3' target="blank">Wayfair</a></h3>
         </div>
     )
 }

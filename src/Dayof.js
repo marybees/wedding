187243f631ad02db8316cceb0926f8cfecb0904d
Mary Baker <meb601@gmail.com>
2021-08-24 09:07:08 -0400

    Update dayof page order and include shuttles link

--- src/Dayof.js
+++ src/Dayof.js
@@ -12,30 +12,36 @@ function Dayof() {
   return (
     <div style={{paddingTop: '8rem'}}>
         <h1 className='pageTitle'>Welcome <span>to</span> Our Wedding</h1>
-        <Row>
-            <Col style={{ backgroundColor: '#E8DDCD'}} className='subtitleDayOf' span={12} align='center'>
-            <Link to='/home'><h2>Play <span>Cocktail Hour(s)</span></h2></Link>
-            <Link to='/home'><TrophyOutlined /></Link>
+        <Row className='dayOf'>
+            <Col style={{ backgroundColor: '#E8DDCD'}} className='subtitleDayOf' s={24} lg={12} align='center'>
+            <Link to='/shuttles'><h2>Shuttles</h2></Link>
+            <CalendarOutlined />
             </Col>
-            <Col style={{ backgroundColor: '#E3D9CA'}} className='subtitleDayOf' span={12} align='center'>
-            <Link to='/seat'><h2>Find <span>Your</span> Seat</h2></Link>
-            <Link to='/seat'><UserOutlined /></Link>
-            </Col>
-        </Row>
-        <Row>
-        <Col style={{ backgroundColor: '#E9E2D7'}} className='subtitleDayOf' span={12} align='center'>
-          <h2>Share <span>the</span> Love</h2>
-          <a href='https://www.instagram.com/explore/tags/willewemaryme/' target='blank'><InstagramOutlined /></a>
-          <a style={{ marginTop: '1rem'}} href='https://www.instagram.com/explore/tags/willewemaryme/' target='blank'>#willewemaryme</a>
-        </Col>
-        <Col style={{ backgroundColor: '#E7DED4'}} className='subtitleDayOf' span={12} align='center'>
-        <h2>Schedule</h2>
+            <Col style={{ backgroundColor: '#E3D9CA'}} className='subtitleDayOf' s={24} lg={12} align='center'>
+            <h2>Schedule</h2>
         <CalendarOutlined />
           <p style={{ marginTop: '2rem'}}>2:00 PM: Ceremony</p>
           <p>3:00 PM: Cocktail Hour(s)</p>
           <p>5:00 PM: Dinner</p>
           <p>6:00 PM: World Premier Band</p>
           <p>10:00 PM: The Lost Tapes Radio Show DJ Set featuring DJ KJG & DJ Wash</p>
+            </Col>
+        </Row>
+        <Row className='dayOf'>
+        <Col style={{ backgroundColor: '#E9E2D7'}} className='subtitleDayOf' s={24} lg={12} align='center'>
+        <Link to='/home'><h2>Play <span>Cocktail Hour(s)</span></h2></Link>
+            <Link to='/home'><TrophyOutlined /></Link>
+        </Col>
+        <Col style={{ backgroundColor: '#E7DED4'}} className='subtitleDayOf' s={24} lg={12} align='center'>
+        <Link to='/seat'><h2>Find <span>Your</span> Seat</h2></Link>
+        <Link to='/seat'><UserOutlined /></Link>
+        </Col>
+      </Row>
+      <Row>
+        <Col style={{ backgroundColor: '#E9E2D7'}} className='subtitleDayOf' span={24} align='center'>
+            <h2>Share <span>the</span> Love</h2>
+            <a href='https://www.instagram.com/explore/tags/willewemaryme/' target='blank'><InstagramOutlined /></a>
+            <a style={{ marginTop: '1rem'}} href='https://www.instagram.com/explore/tags/willewemaryme/' target='blank'>#willewemaryme</a>
         </Col>
       </Row>
     </div>
